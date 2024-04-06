ba959e5d550fb75012633929ea2a33bd8d53d273
kanekoyuya <kaneko@otani-trading.com>
2024-04-06 15:22:32 +0900

    AprilSixth

--- api/cruds/task.py
+++ api/cruds/task.py
@@ -4,7 +4,7 @@ import api.models.task as task_model
 import api.schemas.task as task_schema
 
 def create_task(db:Session,task_create:
-                task_schema.Task_Create) -> task_model.Task:
+                task_schema.TaskCreate) -> task_model.Task:
     task = task_model.Task(**task_create.dict())
     db.add(task)
     db.commit()
